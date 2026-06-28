1d858e7a3d93f967c92a37a00527db7aeb683a38
Michael Beebower <3438663+beebus@users.noreply.github.com>
2026-06-27 22:08:41 -0500

    Updated README.md, app.js, the .spec.js test files, and package.json to use the yarn package manager.

--- spec/routes/updateItem.spec.js
+++ spec/routes/updateItem.spec.js
@@ -1,8 +1,5 @@
-import { test } from 'jest-snapshot';
-
 const db = require('../../src/persistence');
 const updateItem = require('../../src/routes/updateItem');
-const jest = require('jest');
 const ITEM = { id: 12345 };
 
 jest.mock('../../src/persistence', () => ({
